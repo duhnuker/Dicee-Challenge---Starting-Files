ac13fd9a18fbda2c0cb5d1835231c209e9b7bd5a
Dinuka <dinukanh@gmail.com>
2024-04-11 22:45:12 +1000

    Completed

---
+++ script.js
@@ -0,0 +1,22 @@
+//Dice 1
+
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+const img1 = document.querySelectorAll('img')[0];
+img1.setAttribute("src", randomDiceImage1);
+
+//Dice 2
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+const img2 = document.querySelectorAll('img')[1];
+img2.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "Player 1 Wins";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector('h1').innerHTML = "Player 2 Wins";
+} else {
+    document.querySelector('h1').innerHTML = "Draw!";
+
+}
